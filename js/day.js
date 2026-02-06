9c6fc3fe53a044e0cc1dd32757909f48bb3d4330
Aniruddh <c.aniruddh412@gmail.com>
2026-02-06 18:25:41 +0530

    Testing day.js 2

--- js/day.js
+++ js/day.js
@@ -26,20 +26,6 @@ export function getAppDayKey(date = new Date()) {
 
 
 /**
- * Returns internal day key: YYYY-MM-DD
- */
-export function getAppDayKey(date = new Date()) {
-  const d = getAppDate(date);
-
-  const year = d.getFullYear();
-  const month = String(d.getMonth() + 1).padStart(2, "0");
-  const day = String(d.getDate()).padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-}
-
-
-/**
  * Formats a date for UI: DD-MM-YYYY
  */
 export function formatDateForUI(date = new Date()) {
